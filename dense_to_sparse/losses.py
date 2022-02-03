d18dd80e1b6b25d760b457272ff6a27b212aa053
Thong Nguyen <nguyenthacthong@gmail.com>
2022-02-03 14:54:50 +0100

    fix bugs

--- dense_to_sparse/losses.py
+++ dense_to_sparse/losses.py
@@ -118,7 +118,7 @@ class Dense2SparseLoss(nn.Module):
         elif self.reg_type == "l0":
             active_probs = [rep["active_prob"] for rep in reps]
             sparsity_query = torch.sum(active_probs[0].mean(dim=0)**2)
-            sparsity_doc = (torch.sum(active_probs[1].mean(dim=0)**2) + orch.sum(active_probs[2].mean(dim=0)**2))/2
+            sparsity_doc = (torch.sum(active_probs[1].mean(dim=0)**2) + torch.sum(active_probs[2].mean(dim=0)**2))/2
             sparsity = self.lambda_sparse_query*sparsity_query + self.lambda_sparse_doc*sparsity_doc
         else:
             pass
